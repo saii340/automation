2a91b59cd9997acf0a7f1163d2b666454fe4db03
saii340 <111050937+saii340@users.noreply.github.com>
2023-01-24 10:25:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-region = "us-west-2"
-access_key = "AKIA6M5LUYKYHN5EIS7J"
-secret_key = "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"
+  region = "us-west-2"
+  access_key = "AKIA6M5LUYKYHN5EIS7J"
+  secret_key = "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"
 }
 resource "aws_instance" "key" {
-ami = "ami-0ceecbb0f30a902a6"
-instance_type = "t2.micro"
-tags = {
-Name = "java"
+  ami = "ami-0ceecbb0f30a902a6"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "java"
+  }
 }
