db26abcfbfca1ea8f8de3c380338e3653b379132
saii340 <111050937+saii340@users.noreply.github.com>
2023-01-24 11:12:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   access_key = "AKIA6M5LUYKYHN5EIS7J"
   secret_key = "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"
 }
-resource "aws_instance" "key" {
+resource "aws_instance" "ubuntu" {
   ami = "ami-0ceecbb0f30a902a6"
   instance_type = "t2.micro"
   tags = {
