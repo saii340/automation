7dc599138bcbc7f3781bf21df1f7f3e7afb6f52f
saii340 <111050937+saii340@users.noreply.github.com>
2023-01-24 18:52:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
 provider "aws" {
-  region = "us-west-2"
-  access_key = "AKIA6M5LUYKYHN5EIS7J"
-  secret_key = "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"
+    region = "us-west-2"
+    access_key = "AKIA6M5LUYKYHN5EIS7J"
+    secret_key = "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"
 }
 
-resource "aws_instance" "putty" {
-  ami = "ami-0ceecbb0f30a902a6"
-  instance_type = "t2.micro"
-  tags = {
-    "name" = "java"
-  }
+resource "aws_instance" "ubuntu" {
+    ami = "ami-00d8a762cb0c50254"
+    instance_type = "t2.micro"
+    subnet_id="subnet-049687eef0c714710"
+    security_groups=["sg-0928d4152418606f5"]
+    tags = {
+        "name" = "instance"
+    }  
+    
 }
