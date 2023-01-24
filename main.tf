a005c53111c0d5d0345cdb8d530e2e610ba64ebe
saii340 <111050937+saii340@users.noreply.github.com>
2023-01-24 15:10:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-provider "aws" {
+provider "aws"{
   region = "us-west-2"
   access_key = "AKIA6M5LUYKYHN5EIS7J"
   secret_key = "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"
 }
 
-resource "aws_instance" "putty" {
+resource "aws_instance" "putty"{
   ami = "ami-095413544ce52437d"
   instance_type = "t2.micro"
   tags = {
