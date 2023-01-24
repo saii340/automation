5f158ff51a3129d7156b97a53a5510e6d071ca98
saii340 <111050937+saii340@users.noreply.github.com>
2023-01-24 16:05:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,11 @@ provider "aws" {
   access_key = "AKIA6M5LUYKYHN5EIS7J"
   secret_key = "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"
 }
-resource "aws_instance" "putty" 
-  ami = "ami-095413544ce52437d"
+
+resource "aws_instance" "putty" {
+  ami = "ami-0ceecbb0f30a902a6"
   instance_type = "t2.micro"
   tags = {
     "name" = "java"
   }
+}
