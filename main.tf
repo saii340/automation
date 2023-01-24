5969bd22db62680b76c69709f52dd14fd864cc22
saii340 <111050937+saii340@users.noreply.github.com>
2023-01-24 22:35:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
     ami = "ami-00d8a762cb0c50254"
     instance_type = "t2.micro"
-    subnet_id="subnet-09a45f1fc1523c1fc"
+    subnet_id="subnet-03b44739dd8068ad7"
     security_groups=["sg-0928d4152418606f5"]
     tags = {
         "name" = "instance"
