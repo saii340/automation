5bdcaf2d814eed2c81d090f805053514621b85aa
saii340 <111050937+saii340@users.noreply.github.com>
2023-01-27 11:12:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-    region = "us-west-1"
+    region = "us-east-1"
     access_key = "AKIATPY2YNP4YXLPATKJ"
     secret_key = "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"
 }
 
 resource "aws_instance" "ubuntu" {
-    ami = "ami-00d8a762cb0c50254"
+    ami = "ami-0c544bda9765444c2"
     instance_type = "t2.micro"
-    subnet_id="subnet-070c28363782d3866"
-    security_groups=["sg-00ec4cd9debac6427"]
+    subnet_id="subnet-076a0504a53b428a8"
+    security_groups=["sg-00eea6b40fc434644"]
     tags = {
-        "name" = "instance"
+        "Name" = "instance"
     }  
     
 }
